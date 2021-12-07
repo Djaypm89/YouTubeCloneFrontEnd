048394e4a733e31f2b6f0688b6559625d5f504ed
Djaypm89 <djaypm89@gmail.com>
2021-12-07 15:15:43 -0500

    search video operates, array logs to console.  apiKey added to git ignore and properly injects to axios url.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Component } from 'react';
 import SearchBar from "./Components/SearchNavBar/SearchBar";
+import apiKey from "./ApiKey";
 
 
-class App extends Component {
-    constructor (props){
-        super(props);
-        this.state = {
-            searchCriteria: ""
-        }
+const App = (props) => {
+    const [searchCriteria, setSearchCriteria] = useState("");
 
-    }
+    useEffect(() => {getVideos()}, [searchCriteria]) 
 
+ 
 
-componentDidMount() {
-    this.getVideos();
+ const handleSubmit = (criteria) => {
+    setSearchCriteria(criteria);
 }
 
-handleSubmit = (criteria) => {
-    this.setState({searchCriteria: criteria});
-}
 
-async getVideos() {
+
+// console.log above
+
+const getVideos = async () => {
 
     try{
-      let response = await axios.get("https://www.googleapis.com/youtube/v3/search?q={SEARCH QUERY HERE}&key={API KEY HERE} ");
+      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchCriteria}&key=${apiKey}`);
     //   let response = await axios.get("localhost: 3001");
       console.log(response)
 
@@ -36,12 +33,12 @@ async getVideos() {
 
 
 
-  render() {
-      return (
-        <div>
-          <SearchBar handleSubmit={this.handleSubmit} />
-        </div>
-      )};
+  return (
+    <div>
+      <SearchBar handleSubmit={handleSubmit} />
+    </div>
+  );
+ 
 
 }
 
@@ -52,3 +49,48 @@ export default App;
 
 
 //https://www.googleapis.com/youtube/v3/search?q={SEARCH QUERY HERE}&key={API KEY HERE} 
+
+
+
+// class App extends Component {
+//     constructor (props){
+//         super(props);
+//         this.state = {
+//             searchCriteria: ""
+//         }
+
+//     }
+
+
+// componentDidMount() {
+//     this.getVideos();
+// }
+
+// handleSubmit = (criteria) => {
+//     this.setState({searchCriteria: criteria});
+// }
+
+// // console.log above
+
+// async getVideos() {
+
+//     try{
+//       let response = await axios.get("https://www.googleapis.com/youtube/v3/search?q={SEARCH QUERY HERE}&key={API KEY HERE} ");
+//     //   let response = await axios.get("localhost: 3001");
+//       console.log(response)
+
+//     }catch (error) {
+//       console.log("API request error");
+//     }
+//   } 
+
+
+
+//   render() {
+//       return (
+//         <div>
+//           <SearchBar handleSubmit={this.handleSubmit} />
+//         </div>
+//       )};
+
+// }
